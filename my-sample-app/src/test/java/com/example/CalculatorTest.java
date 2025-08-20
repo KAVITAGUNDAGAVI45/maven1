1a490252692359b8d5ecd78d2c539975162475e9
KAVITAGUNDAGAVI45 <gundagavikavita6@gmail.com>
2025-08-20 11:48:55 +0530

    Update CalculatorTest.java

--- my-sample-app/src/test/java/com/example/CalculatorTest.java
+++ my-sample-app/src/test/java/com/example/CalculatorTest.java
@@ -7,7 +7,7 @@ public class CalculatorTest {
     @Test
     public void testAdd() {
         Calculator calc = new Calculator();
-        assertEquals(15, calc.add(20, 5));
+        assertEquals(15, calc.add(5, 5));
     }
 
     @Test
